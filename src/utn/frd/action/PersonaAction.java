702432f201445cceafb7e7222329f00c5ffd8076
JoacoSanchez <joaqosanchez@gmail.com>
2017-04-14 01:39:58 -0300

    Entrega2
    En el textfield de "Nombre" admito que el usuario pueda ingresar numeros
    ademas de letras (y otros simbolos)

--- src/utn/frd/action/PersonaAction.java
+++ src/utn/frd/action/PersonaAction.java
@@ -54,12 +54,26 @@ public class PersonaAction extends ActionSupport {
 		personas = PersistentManager.getInstance(); 
 		int edad = 0;
 		Persona p;
+		//tuve que agregar estas variables locales auxiliares para asegurarme que no se deje un espacio vacio
+		int aux1;
+		int aux2;
+		int aux3;
 		try{
 			edad = Integer.parseInt(age);
+			aux1 = name.length();
+			aux2 = gender.length();
+			aux3 = 1 / aux2;
+			aux3 = 1 / aux1;
 		}catch(Exception e){
-			addActionError("ocurrió un error con la edad");
+			addActionError("Ocurrió un error");
 			return ERROR;
 		}
+		/*
+		if (name=="" || gender==""){
+			addActionError("Hubieron casillas sin rellenar");
+			return ERROR;
+		}
+		*/
 		p = new Persona(personas.size(), name, edad, gender);
 		personas.add(p);
 		setName("");
